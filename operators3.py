fd640217400737488c32368b6fe6439921f31ff3
Madhvendra3 <rajawatbunty@gmail.com>
2026-02-22 18:28:30 +0000

    Add files via upload

---
+++ operators3.py
@@ -0,0 +1,18 @@
+a = 5
+b = 4 
+c = 6
+d = 3 
+e = 1
+
+e = (a-b)*(c/d)
+print("value of e is:", e)
+
+name = "bob"
+age = 5
+
+if name == "bob" or name == "bob" and age <= 2:
+    print("welcome")
+else:
+    print("bye")    
+
+
