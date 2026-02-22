13a5074daceecc0f9d40d0246f4c989a952b64ce
Madhvendra3 <rajawatbunty@gmail.com>
2026-02-22 18:37:28 +0000

    Add files via upload

---
+++ modulus.py
@@ -0,0 +1,8 @@
+num = int(input("enter number"))
+num2 = int(input("enter number"))
+
+if num%num2 == 0:
+    print("number is divisible")
+else:
+    print("number is not divisible, pick new number.")
+        
